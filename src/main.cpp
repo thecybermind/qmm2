ce6a5a08287c759f3e8efd4ca1ae8e14758bf7c6
Kevin Masterson <cybermind@gmail.com>
2025-03-07 20:30:18 -0500

    update .gitignore, add msvc project files, add initial source files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+QMM2 - Q3 MultiMod 2
+Copyright 2025
+https://github.com/thecybermind/qmm2/
+3-clause BSD license: https://opensource.org/license/bsd-3-clause
+
+Created By:
+    Kevin Masterson < cybermind@gmail.com >
+
+*/
+
+#include "qmmapi.h"
