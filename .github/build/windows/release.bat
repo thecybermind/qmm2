23a92473ab537b2d46a9f9025a8506fd7e083337
Kevin Masterson <cybermind@gmail.com>
2025-03-16 16:39:01 -0400

    using microsoft/setup-msbuild action

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,5 +1,1 @@
-cd /d "C:\Program Files\Microsoft Visual Studio\"
-dir /s vcvarsamd64_x86.bat
-
-rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
-rem msbuild .\msvc\qmm2.vcxproj /p:Configuration=Release
+msbuild .\msvc\qmm2.vcxproj /p:Configuration=Release
