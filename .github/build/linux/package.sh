e131523a406222e5a448f1ba59c48997dd1b4ee9
Kevin Masterson <cybermind@gmail.com>
2025-03-16 15:55:16 -0400

    ready for testing

--- .github/build/linux/package.sh
+++ .github/build/linux/package.sh
@@ -6,7 +6,7 @@ cp README.md ./package/
 cp LICENSE ./package/
 cp bin/release/qmm2.so ./package/
 cd package
-if [[ -z "${GITHUB_TOKEN}" ]]; then
+if [ -z "${GITHUB_TOKEN}" ]; then
   tar zcf ../qmm2-latest.tar.gz *
 else
   tar zcf ../qmm2-${{ github.ref_name }}.tar.gz *
@@ -17,9 +17,9 @@ rm -rf package
 mkdir -p package
 cp README.md ./package/
 cp LICENSE ./package/
-cp bin/debug/qmm2.so ./package/
+cp bin/debug/qmm2-dbg.so ./package/
 cd package
-if [[ -z "${GITHUB_TOKEN}" ]]; then
+if [ -z "${GITHUB_TOKEN}" ]; then
   tar zcf ../qmm2-latest-debug.tar.gz *
 else
   tar zcf ../qmm2-${{ github.ref_name }}-debug.tar.gz *
