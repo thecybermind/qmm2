78bb0c1e644bf97803d4b56a50fce29026272b19
Kevin Masterson <cybermind@gmail.com>
2025-03-16 16:29:33 -0400

    test

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,4 +1,4 @@
-cd "C:\Program Files\Microsoft Visual Studio\"
+cd /d "C:\Program Files\Microsoft Visual Studio\"
 dir /s vcvarsamd64_x86.bat
 
 rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
