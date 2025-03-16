eba9101180e6ed9cafc85285b68686c3bade7dbb
Kevin Masterson <cybermind@gmail.com>
2025-03-16 18:24:05 -0400

    add qmm2.json to release artifacts

--- .github/build/linux/package.sh
+++ .github/build/linux/package.sh
@@ -4,5 +4,6 @@ cd package
 rm *
 cp ../README.md ./
 cp ../LICENSE ./
+cp ../qmm2.json ./
 cp ../bin/release/qmm2.so ./
 cd ..
--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -3,6 +3,7 @@ cd package
 del /q *
 rem copy ..\README.md .\
 rem copy ..\LICENSE .\
+rem copy ..\qmm2.json .\
 copy ..\bin\Release\qmm2.dll .\
 copy qmm2.dll jampgamex86.dll
 powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_ja.pk3"
