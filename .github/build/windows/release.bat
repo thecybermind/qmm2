4b2a2be7cc380864ac3791bb2b69144906a4c2da
Kevin Masterson <cybermind@gmail.com>
2025-03-16 16:28:32 -0400

    this is so annoying. why can't i test this crap another way

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,2 +1,5 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
-msbuild .\msvc\qmm2.vcxproj /p:Configuration=Release
+cd "C:\Program Files\Microsoft Visual Studio\"
+dir /s vcvarsamd64_x86.bat
+
+rem call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+rem msbuild .\msvc\qmm2.vcxproj /p:Configuration=Release
