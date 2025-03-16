f050a24324d474517284de42abe6b262587c90e9
Kevin Masterson <cybermind@gmail.com>
2025-03-16 17:38:47 -0400

    fix windows package script

--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -3,7 +3,7 @@ cd package
 del /q *
 rem copy ..\README.md .\
 rem copy ..\LICENSE .\
-copy ..\msvc\Release\qmm2.dll .\
+copy ..\bin\Release\qmm2.dll .\
 copy qmm2.dll jampgamex86.dll
 powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_jka.pk3"
 del jampgamex86.dll
