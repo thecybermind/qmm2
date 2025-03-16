41c3c5981a018873a4d7bdf8d51c16b0e74b013a
Kevin Masterson <cybermind@gmail.com>
2025-03-16 17:31:38 -0400

    don't upload readme/license from windows build

--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -1,8 +1,8 @@
 mkdir package
 cd package
 del /q *
-copy ..\README.md .\
-copy ..\LICENSE .\
+rem copy ..\README.md .\
+rem copy ..\LICENSE .\
 copy ..\msvc\Release\qmm2.dll .\
 copy qmm2.dll jampgamex86.dll
 powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_jka.pk3"
