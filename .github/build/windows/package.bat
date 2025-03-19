a043b5df2ab7ac47635a381b3390bb6d852bb6d9
Kevin Masterson <cybermind@gmail.com>
2025-03-19 16:58:57 -0400

    update readme to add wiki link for updated documentation. will likely remove it later. also renamed .pk3 file in windows packager to match new "jamp" code for Jedi Academy

--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -6,6 +6,6 @@ rem copy ..\LICENSE .\
 rem copy ..\qmm2.json .\
 copy ..\bin\Release\qmm2.dll .\
 copy qmm2.dll jampgamex86.dll
-powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_ja.pk3"
+powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_jamp.pk3"
 del jampgamex86.dll
 cd ..
