a3827bddd47b72d4a049ad44e603219beb74ac6d
Kevin Masterson <cybermind@gmail.com>
2025-03-21 20:00:48 -0400

    fix jamp pk3 packaging

--- .github/build/windows/package.bat
+++ .github/build/windows/package.bat
@@ -1,11 +1,12 @@
 mkdir package
-cd package
+pushd package
 del /q *
 rem copy ..\README.md .\
 rem copy ..\LICENSE .\
 rem copy ..\qmm2.json .\
 copy ..\bin\Release\qmm2.dll .\
 copy qmm2.dll jampgamex86.dll
-powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_jamp.pk3"
+powershell "Compress-Archive -Path jampgamex86.dll -Destination zzz_qmm_jamp.zip"
+ren zzz_qmm_jamp.zip zzz_qmm_jamp.pk3
 del jampgamex86.dll
-cd ..
+popd
