33ac57cdfac8936ec374aac7ab06753cf495a31d
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 22:16:31 +0200

    add code to exit build with errorlevel

--- .github/build/windows/debug.bat
+++ .github/build/windows/debug.bat
@@ -1,2 +1,4 @@
 msbuild .\msvc\qmm2.vcxproj /p:Configuration=Debug /p:Platform=x86
+if errorlevel 1 exit /b errorlevel
 msbuild .\msvc\qmm2.vcxproj /p:Configuration=Debug /p:Platform=x64
+if errorlevel 1 exit /b errorlevel
